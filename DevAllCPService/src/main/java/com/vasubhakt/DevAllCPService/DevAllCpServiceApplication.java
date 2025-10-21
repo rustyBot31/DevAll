291f1c691788dca13e46219162a59f585835c91a
Swastik Bose <swastikbose31@gmail.com>
2025-10-21 14:55:35 +0530

    cp service created

---
+++ DevAllCPService/src/main/java/com/vasubhakt/DevAllCPService/DevAllCpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vasubhakt.DevAllCPService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevAllCpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevAllCpServiceApplication.class, args);
+	}
+
+}
---
+++ DevAllCPService/src/test/java/com/vasubhakt/DevAllCPService/DevAllCpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasubhakt.DevAllCPService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevAllCpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
