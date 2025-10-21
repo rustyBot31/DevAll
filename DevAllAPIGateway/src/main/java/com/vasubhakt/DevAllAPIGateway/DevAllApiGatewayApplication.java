1df61678346aa5fce9a94108a50312b50cd62968
Swastik Bose <swastikbose31@gmail.com>
2025-10-21 23:33:40 +0530

    api gateway created

---
+++ DevAllAPIGateway/src/main/java/com/vasubhakt/DevAllAPIGateway/DevAllApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.vasubhakt.DevAllAPIGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevAllApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevAllApiGatewayApplication.class, args);
+	}
+
+}
---
+++ DevAllAPIGateway/src/test/java/com/vasubhakt/DevAllAPIGateway/DevAllApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasubhakt.DevAllAPIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevAllApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
