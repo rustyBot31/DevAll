751aa3bc45807af1e4e7eb30d682609c15c9efe9
Swastik Bose <swastikbose31@gmail.com>
2025-10-21 15:20:21 +0530

    update

--- DevAllAuthService/src/main/java/com/vasubhakt/DevAllAuthService/DevAllAuthServiceApplication.java
+++ DevAllAuthService/src/main/java/com/vasubhakt/DevAllAuthService/DevAllAuthServiceApplication.java
@@ -3,7 +3,7 @@ package com.vasubhakt.DevAllAuthService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.vasubhakt.DevAllAuthService")
+@SpringBootApplication
 public class DevAllAuthServiceApplication {
 
 	public static void main(String[] args) {
