f3c82079f5744ca5de29c9e9086385e9a3478093
Swastik Bose <swastikbose31@gmail.com>
2025-10-29 16:25:29 +0530

    minor fix

--- DevAllCPService/src/main/java/com/vasubhakt/DevAllCPService/Fetch/ACFetch.java
+++ DevAllCPService/src/main/java/com/vasubhakt/DevAllCPService/Fetch/ACFetch.java
@@ -1,7 +1,6 @@
 package com.vasubhakt.DevAllCPService.Fetch;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
