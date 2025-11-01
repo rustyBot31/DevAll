f8987c075232e90b31f4a6441abcf4546980a040
Swastik Bose <swastikbose31@gmail.com>
2025-11-01 14:13:34 +0530

    project service initialized

---
+++ DevAllProjectService/src/main/java/com/vasubhakt/DevAllProjectService/DevAllProjectServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vasubhakt.DevAllProjectService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevAllProjectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevAllProjectServiceApplication.class, args);
+	}
+
+}
