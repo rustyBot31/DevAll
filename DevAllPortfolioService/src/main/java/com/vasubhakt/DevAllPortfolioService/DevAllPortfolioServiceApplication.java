f4b9ded73ab0fd77851a4da55e102e73ae3906c3
Swastik Bose <swastikbose31@gmail.com>
2025-11-12 01:18:06 +0530

    portfolio service initialized

---
+++ DevAllPortfolioService/src/main/java/com/vasubhakt/DevAllPortfolioService/DevAllPortfolioServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vasubhakt.DevAllPortfolioService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevAllPortfolioServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevAllPortfolioServiceApplication.class, args);
+	}
+
+}
---
+++ DevAllPortfolioService/src/test/java/com/vasubhakt/DevAllPortfolioService/DevAllPortfolioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasubhakt.DevAllPortfolioService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevAllPortfolioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
